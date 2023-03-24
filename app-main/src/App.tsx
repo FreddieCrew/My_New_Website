2a2f9b07e1f73e0261de593ffac99af3f77c4048
FreddieCrew <120303127+FreddieCrew@users.noreply.github.com>
2023-03-24 08:32:56 -0300

    Add files via upload

---
+++ app-main/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="App-header">
+        <h2>
+          Welcome to my website
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
